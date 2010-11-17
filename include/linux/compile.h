214744ca625e8f995c6f1d7b7e46f08cc6ca21a4
Soaa- <soaa@soaa-laptop.(none)>
2010-11-16 21:01:23 -0500

    Merge upstream changes.

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue Nov 16 20:57:25 EST 2010"
+#define LINUX_COMPILE_TIME "20:57:25"
+#define LINUX_COMPILE_BY "soaa"
+#define LINUX_COMPILE_HOST "soaa-laptop"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
